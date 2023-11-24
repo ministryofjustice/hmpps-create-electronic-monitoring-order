2e1a5d809dc96c245bd98b6daa9834e94183f8f5
devarrowsmith <devarrowsmith@users.noreply.github.com>
2023-11-24 14:30:12 +0000

    updating project name after deployment from template repository

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscreateelectronicmonitoringorder/HmppsCreateElectronicMonitoringOrder.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.hmppscreateelectronicmonitoringorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication()
+class HmppsCreateElectronicMonitoringOrder
+
+fun main(args: Array<String>) {
+  runApplication<HmppsCreateElectronicMonitoringOrder>(*args)
+}
