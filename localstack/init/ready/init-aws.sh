56231562e4506352268a2096b0d864e912dabca1
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-02-22 15:21:23 +0000

    Configure localstack init script to create SQS for testing.

--- localstack/init/ready/init-aws.sh
+++ localstack/init/ready/init-aws.sh
@@ -4,3 +4,6 @@
 
 # Create S3 bucket for testing
 awslocal s3api create-bucket --bucket cemo-test-bucket
+
+# Create SQS for testing
+awslocal sqs create-queue --queue-name cemo-test-bucket --region eu-west-1
