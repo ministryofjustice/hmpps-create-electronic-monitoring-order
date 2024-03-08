d09ba4fca33cf20ae7a7ece76dc213382020e472
Dave Arrowsmith <devarrowsmith@gmail.com>
2024-03-08 13:00:46 +0000

    Database integration tests run successfully in CircleCI. (#4)
    * Debugging integration tests.
    
    * Add localstack_tag parameter to java_localstack_postgres executor in circleci config.yaml.
    
    * Add a postgres ready check to circleci config.
    
    * Linting.
    
    * Rename postgres integration test credentials.
    
    * Database integration test now passes locally and in CircleCI. Add comment to config.yaml.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscreateelectronicmonitoringorder/integration/DatabaseIntegrationTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscreateelectronicmonitoringorder/integration/DatabaseIntegrationTests.kt
@@ -2,27 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppscreateelectronicmonitoringorder.integr
 
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 import java.sql.DriverManager
 
-@Testcontainers
 class DatabaseIntegrationTests {
-  companion object {
-    @JvmStatic
-    @Container
-    val postgresContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:16.2")
-      .apply {
-        withDatabaseName("testdatabase")
-        withUsername("testuser")
-        withPassword("testpassword")
-      }
-  }
-
-  private val url: String = postgresContainer.jdbcUrl
-  private val user: String = postgresContainer.username
-  private val password: String = postgresContainer.password
+  private val url = "jdbc:postgresql://localhost:5432/cemo-database"
+  private val user = "cemo-database"
+  private val password = "cemo-database"
 
   @Test
   fun `Service can connect to database`() {
