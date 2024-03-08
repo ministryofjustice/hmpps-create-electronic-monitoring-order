217e099685fbeb087f0112191852a71a7374a649
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-03-08 11:45:01 +0000

    Linting.

--- postgres-check.sh
+++ postgres-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 checked=0
-while [ ! $(netstat -na | grep '0.0.0.0.5432 ') ] && [ $checked -lt 10 ];
+while [ ! "$(netstat -na | grep '0.0.0.0.5432 ')" ] && [ $checked -lt 10 ];
 do
   echo 'Checked port 5432 to see if Postgres is up - not yet'
   sleep 5
